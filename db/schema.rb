4e3e64ffd5c5024ffcc2fff3ce9a6bc65370e31c
Rohit R Poduval <rohitrp2003@gmail.com>
2015-09-22 13:09:07 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921235848) do
+ActiveRecord::Schema.define(version: 20150920175107) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
